47402b099eed8ad52e02d742eb746f448a1fe14c
Alex Sorichetti <107128425+asorichetti@users.noreply.github.com>
2025-03-21 14:27:19 -0400

    FEAT: A4p2 Start
    begin assignment 4 part 2 with header comment and
    file imports as well as having set up a array for
    image locations

---
+++ assignment_4/assignment_4p2/main.js
@@ -0,0 +1,18 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+const imageHolder = ["./images/pic1.jpg", "./images/pic2.jpg", "./images/pic3", "./images/pic4", "./images/pic5"];
+/* Declaring the alternative text for each image file */
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
