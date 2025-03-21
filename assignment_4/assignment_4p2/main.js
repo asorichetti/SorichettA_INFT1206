29675a67f14881b933e4c477eeb0139fd69b92ec
Alex Sorichetti <107128425+asorichetti@users.noreply.github.com>
2025-03-21 14:46:28 -0400

    FEAT: Image Gallery Handling
    Coded javascript to handle image gallery movement
    and allow user to click through the image gallery

--- assignment_4/assignment_4p2/main.js
+++ assignment_4/assignment_4p2/main.js
@@ -5,14 +5,18 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imageHolder = ["./images/pic1.jpg", "./images/pic2.jpg", "./images/pic3", "./images/pic4", "./images/pic5"];
+const imageHolder = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"];
 /* Declaring the alternative text for each image file */
-
+const imageAlts = ["A close up image of a human eye", "Flowing sandstone similar to waves", "A close up of purple and white flowers", "Egyptian stone artwork and hieroglyphs", "A butterfly on a green leaf"];
 /* Looping through images */
-
+for (let i = 0; i<5; i++){
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', imageHolder[i]);
+newImage.setAttribute('alt', imageAlts[i]);
 thumbBar.appendChild(newImage);
-
+newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+});
+}
 /* Wiring up the Darken/Lighten button */
