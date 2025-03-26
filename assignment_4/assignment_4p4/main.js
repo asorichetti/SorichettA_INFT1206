a1028ad49bf7077a1eeb73778cb6ef3f86b8b56e
Alex Sorichetti <107128425+asorichetti@users.noreply.github.com>
2025-03-26 11:01:23 -0400

    FEAT: Evil Ball and Counter Update
    Updated with Evil Ball and counter which tracks
    how many balls are on the screen.

--- assignment_4/assignment_4p4/main.js
+++ assignment_4/assignment_4p4/main.js
@@ -1,5 +1,9 @@
 // setup canvas
 
+
+const para = document.querySelector('p');
+let count = 0;
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -73,6 +77,72 @@ class Ball extends Shape {
       
   }
   
+  class EvilCircle extends Shape {
+    constructor(x,y){
+      super(x,y,15,15);
+      this.color = "white";
+      this.size = 10;
+
+      window.addEventListener('keydown', (l) =>{
+        switch(l.key){
+          case 'a':
+            this.x -= this.velX;
+            break;
+          case 'd':
+            this.x += this.velX;
+            break;
+          case 'w':
+            this.y -= this.velY;
+            break;
+          case 's':
+            this.y += this.velY;
+            break;
+        }
+      });
+    }
+
+    draw(){
+      ctx.beginPath();
+      ctx.strokeStyle = this.color;
+      ctx.lineWidth = 4;
+      ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+      ctx.stroke;
+    }
+
+    checkBounds() {
+
+      if ((this.x - this.size) <= 0){
+        this.x += this.size;
+      }
+      if ((this.y - this.size) <= 0){
+        this.y += this.size;
+      }
+
+      if ((this.x+this.size)>= width){
+        this.x -= this.size;
+      }
+      if ((this.y + this.size) >= height){
+        this.y -= this.size;
+      }
+     
+    }
+
+    collisionDetect() {
+      for (const ball of balls){
+        if (ball.exists){
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+
+          if (distance < this.size + ball.size){
+            ball.exists = false;
+            count--;
+            parseFloat.textContent = 'Ball count: ' + count;
+          }
+        }
+      }
+    }
+  }
   const balls = [];
 
 while (balls.length < 25) {
@@ -89,16 +159,26 @@ while (balls.length < 25) {
   );
 
   balls.push(ball);
+  count++;
+  para.textContent = 'Ball count: ' + count;
 }
+
+const evilBall = new EvilCircle(random(0, width), random(0, height));
 function loop() {
     ctx.fillStyle = "rgb(0 0 0 / 25%)";
     ctx.fillRect(0, 0, width, height);
   
     for (const ball of balls) {
+      if (ball.exists){
       ball.draw();
       ball.update();
       ball.collisionDetect();
+      }
     }
+
+    evilBall.draw();
+    evilBall.checkBounds();
+    evilBall.collisionDetect();
   
     requestAnimationFrame(loop);
   }
